2c00e7ab96f8073c686af844c7c9ad70adb06a0b
Felipe Cechin Mello <ficechin@hotmail.com>
2017-03-22 17:18:42 -0300

    Arquivos de base do sistema adicionados.

---
+++ src/curso/hibernate/teste/UsuarioTeste.java
@@ -0,0 +1,26 @@
+package curso.hibernate.teste;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import curso.hibernate.modelo.Usuario;
+
+
+public class UsuarioTeste {
+	public static void main(String[] args) {
+		
+		Usuario usuario = new Usuario();
+		//usuario.setId(3);
+		usuario.setNome("Usuário Teste 4");
+		
+		SessionFactory factory = new Configuration().configure().buildSessionFactory();
+		Session session = factory.openSession();
+		session.beginTransaction();
+		
+		session.save(usuario);
+		session.getTransaction().commit();
+		session.close();
+	}
+
+}
